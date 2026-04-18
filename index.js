a645527445f90da5f2821b4604357e0adfd94db2
Gloria Wanja <gloriaw12198@gmail.com>
2026-04-19 02:48:54 +0300

    completed combineUsers lab

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+function combineUsers(...arrays) {
+  const users = [].concat(...arrays);
 
+  const merge_date = new Date().toISOString().split("T")[0];
 
+  return {
+    users,
+    merge_date
+  };
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
